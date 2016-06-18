56b373d0fc1f410463133b8a4e06b88306112bbb
Pat Myron <pmyron09@gmail.com>
2016-06-18 01:18:17 -0700

    .

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
+        if (event.message) {
 sendMessage(event.sender.id, {text: "It is going to rain"}); // event.message.text
 sendMessage(event.sender.id, {text: "It is going to rain 2"}); // event.message.text
 sendMessage(event.sender.id, {text: "1" + String(event.message)});
