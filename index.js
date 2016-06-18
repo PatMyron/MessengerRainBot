9a7351a792c0176c90478663676f541843e54dba
Pat Myron <pmyron09@gmail.com>
2016-06-18 09:16:13 -0700

    .

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        sendMessage(event.sender.id, {text: "Hi Pat"}); // event.message.text
         if (event.message && event.message.text) {
-		sendMessage(event.sender.id, {text: "Send your location"}); // event.message.text
+		sendMessage(event.sender.id, {text: "Hi. Send your location"}); // event.message.text
         }
 	else if (event.message && event.message.attachments[0] && event.message.attachments[0].payload) {
 		urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/"
