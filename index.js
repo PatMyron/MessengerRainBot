7746dfab4e19ba24a33904440bc1d2f4f78a5e9a
Pat Myron <pmyron09@gmail.com>
2016-06-18 02:47:46 -0700

    .

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-	if (event.message) {
-                sendMessage(event.sender.id, {text: "Send your location"}); // event.message.text
-
-	}
         if (event.message && event.message.text) {
 		sendMessage(event.sender.id, {text: "Send your location"}); // event.message.text
         }
