5a7c8f1133e722df6dccff09135c756a5bfe5eb0
Pat Myron <pmyron09@gmail.com>
2016-12-27 22:05:06 -0500

    more cleanup

--- index.js
+++ index.js
@@ -27,35 +27,33 @@ app.post('/webhook', function (req, res) {
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.text) {
-        sendMessage(event.sender.id, {text: "Hi. Send your location"}); // event.message.text
-      }
-      else if (event.message && event.message.attachments && event.message.attachments[0] && event.message.attachments[0].payload && event.message.attachments[0].payload.coordinates) {
-        urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/"
-        lat = event.message.attachments[0].payload.coordinates.lat
-        lon = event.message.attachments[0].payload.coordinates.long
-        totUrl = urlBase + String(lat) + "," + String(lon) + ".json"
+      sendMessage(event.sender.id, {text: "Hi. Send your location"}); // event.message.text
+    } else if (event.message && event.message.attachments && event.message.attachments[0] && event.message.attachments[0].payload && event.message.attachments[0].payload.coordinates) {
+      urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/"
+      lat = event.message.attachments[0].payload.coordinates.lat
+      lon = event.message.attachments[0].payload.coordinates.long
+      totUrl = urlBase + String(lat) + "," + String(lon) + ".json"
 
-        request({
-          url: totUrl,
-          json: true
-        }, function (error, response, body) {
+      request({
+        url: totUrl,
+        json: true
+      }, function (error, response, body) {
 
-          if (!error && response.statusCode === 200) {
-            var rain = body.current_observation.precip_1hr_metric
-            if (rain > 0) {
-              sendMessage(event.sender.id, {text: "It's gonna rain. Grab an umbrella!"});
-            }
-            else {
-              sendMessage(event.sender.id, {text: "No rain ahead!"});
-            }
+        if (!error && response.statusCode === 200) {
+          var rain = body.current_observation.precip_1hr_metric
+          if (rain > 0) {
+            sendMessage(event.sender.id, {text: "It's gonna rain. Grab an umbrella!"});
+          } else {
+            sendMessage(event.sender.id, {text: "No rain ahead!"});
           }
-        })
-      } 
-      events = []
-    }
-    req.body.entry[0].messaging = []
-    res.sendStatus(200);
-  });
+        }
+      })
+    } 
+    events = []
+  }
+  req.body.entry[0].messaging = []
+  res.sendStatus(200);
+});
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
