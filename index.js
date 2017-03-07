271cf98ae82dd76efc8d4f2d3128a43b25a33513
Pat Myron <pmyron09@gmail.com>
2017-03-07 03:57:43 -0500

    cleanup

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.post('/webhook', function (req, res) {
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.attachments && event.message.attachments[0] && event.message.attachments[0].payload && event.message.attachments[0].payload.coordinates) {
-      urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/"
-      lat = event.message.attachments[0].payload.coordinates.lat
-      lon = event.message.attachments[0].payload.coordinates.long
-      totUrl = urlBase + String(lat) + "," + String(lon) + ".json"
+      urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/";
+      lat = event.message.attachments[0].payload.coordinates.lat;
+      lon = event.message.attachments[0].payload.coordinates.long;
+      totUrl = urlBase + String(lat) + "," + String(lon) + ".json";
 
       request({
         url: totUrl,
@@ -38,7 +38,7 @@ app.post('/webhook', function (req, res) {
       }, function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
-          var rain = body.current_observation.precip_1hr_metric
+          var rain = body.current_observation.precip_1hr_metric;
           if (rain > 0) {
             sendMessage(event.sender.id, {text: "It's gonna rain. Grab an umbrella!"});
           } else {
@@ -54,7 +54,7 @@ app.post('/webhook', function (req, res) {
     } 
     events = []
   }
-  req.body.entry[0].messaging = []
+  req.body.entry[0].messaging = [];
   res.sendStatus(200);
 });
 
